a7d7eaa2d2204f89b6b99a2ed64a9658ef1fa069
Gilles Dubreuil <gilles@redhat.com>
2015-03-13 13:33:30 +1100

    added a flag for OPM refresh git

--- opm.sh
+++ opm.sh
@@ -1,6 +1,7 @@
 echo "Snippet - install Openstack Puppet Modules"
 opm=$1
 branch=$2
+refresh=$3
 
 function around() {
   # Work around when opm not available in repo
@@ -10,6 +11,12 @@ function around() {
   rpm -ivh "http://download.lab.bos.redhat.com/rel-eng/OpenStack/Foreman/latest/RHEL-6-Server-OS-Foreman/x86_64/os/Packages/$rpm"
 }
 
+function git_get() {
+  git clone $opm '/usr/share/openstack-puppet/modules'
+  cd  '/usr/share/openstack-puppet/modules'
+  git checkout $branch
+}
+
 case $opm in
   rpm)
     echo 'OPM from RPM'
@@ -17,9 +24,12 @@ case $opm in
     ;;
   *)
     echo 'OPM from GIT'
-    [[ ! -d /usr/share/openstack-puppet ]] && mkdir /usr/share/openstack-puppet
-    git clone $opm '/usr/share/openstack-puppet/modules'
-    cd  '/usr/share/openstack-puppet/modules'
-    git checkout $branch
+    if [[ ! -d /usr/share/openstack-puppet ]]
+    then
+      mkdir /usr/share/openstack-puppet
+      git_get
+    else
+      [[ $refresh -eq 1 ]] && git_get
+    fi
     ;;
 esac
