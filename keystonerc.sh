d32e59e966af8cfc6d468f027025d4ab8a1321e7
Gilles Dubreuil <gilles@redhat.com>
2015-03-15 22:39:31 +1100

    added version for keystonerc

--- keystonerc.sh
+++ keystonerc.sh
@@ -2,10 +2,9 @@ echo "Snippet - keystonerc V2"
 password=$1
 token=$2
 public=$3
-version=$4 # '/v2.0'
+version='/v2.0'
 
 cat > /root/keystonerc <<EOD
-export ADMIN_TOKEN=$token
 export OS_USERNAME=admin
 export OS_TENANT_NAME=admin
 export OS_PASSWORD=$password
